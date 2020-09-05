6c1c9c30c1d2295b038ebfc22b2dd0ef4095cfa2
javifs94 <javifs94@gmail.com>
2020-09-05 09:21:02 +0200

    Carousel made it

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const fila = document.querySelector(".carousel-container");
+const peliculas = document.querySelectorAll(".movie");
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+leftArrow.addEventListener("click", () => {
+    fila.scrollLeft -= fila.offsetWidth;
+    const indicadorActivo = document.querySelector(".indicadores .activo");
+    if (indicadorActivo.previousSibling) {
+        indicadorActivo.previousSibling.classList.add("activo");
+        indicadorActivo.classList.remove("activo");
+    }
+});
+
+rightArrow.addEventListener("click", () => {
+    fila.scrollLeft += fila.offsetWidth;
+    const indicadorActivo = document.querySelector(".indicadores .activo");
+    if (indicadorActivo.nextSibling) {
+        indicadorActivo.nextSibling.classList.add("activo");
+        indicadorActivo.classList.remove("activo");
+    }
+});
+
+const pageNum = Math.ceil(peliculas.length / 5);
+for (let i = 0; i < pageNum; i++) {
+    const indicador = document.createElement("button");
+    if (i === 0) {
+        indicador.classList.add("activo");
+    }
+    document.querySelector(".indicadores").appendChild(indicador);
+    indicador.addEventListener("click", (e) =>{
+        fila.scrollLeft = i * fila.offsetWidth;
+        document.querySelector(".indicadores .activo").classList.remove("activo");
+        e.target.classList.add("activo");
+    });
+}
+
+peliculas.forEach((movie) => {
+    movie.addEventListener("mouseenter", (e) => {
+        const elemento = e.currentTarget;
+        setTimeout(() => {
+            peliculas.forEach(movie => movie.classList.remove("hover"));
+            elemento.classList.add("hover");
+        }, 300);
+    });
+});
+
+fila.addEventListener("mouseleave", () => {
+    peliculas.forEach(movie => movie.classList.remove("hover"));
+});
